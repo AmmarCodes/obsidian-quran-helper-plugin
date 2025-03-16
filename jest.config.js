ff54ac98008e89c2b358a31178c47ede99a428c0
Khabir <hakim1khabir2@gmail.com>
2025-03-16 23:40:49 +0000

    fix broken tests for normalizeArabic function

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} **/
-export default {
+module.exports = {
   testEnvironment: "node",
   transform: {
     "^.+\.tsx?$": ["ts-jest",{}],
+  preset: 'ts-jest',
   },
-  preset: "ts-jest",
 };
