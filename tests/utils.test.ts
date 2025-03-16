b04a573da639764e8408362990a739712fb83751
Abdelhakim khabir <89462747+hakkm@users.noreply.github.com>
2025-03-16 22:41:30 +0000

    Refactor tests/utils.test.ts
    Co-authored-by: Ammar Alakkad <am.alakkad@gmail.com>

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,27 +1,14 @@
 import { normalizeArabic } from "../src/utils";
 
 describe("normalizeArabicChars", () => {
-    it("should handle empty strings", () => {
-        expect(normalizeArabic("")).toBe("");
-    });
-
-    it("should handle strings \u0670", () => {
-        expect(normalizeArabic("عَلَىٰ")).toBe("على");
-    });
-
-    it("should handle strings \u0671", () => {
-        expect(normalizeArabic("أَعْلَمُ")).toBe("اعلم");
-    });
-
-    it("should normalize الرَّحْمَٰنِ to الرحمان", () => {
-        expect(normalizeArabic("الرَّحْمَٰنِ")).toBe("الرحمن");
-    });
-
-    it("should normalize اللَّهِ ۗ وَاللَّهُ to الله والله", () => {
-        expect(normalizeArabic("اللَّهِ ۗ وَاللَّهُ")).toBe("الله والله");
-    });
-
-    it("should normalize أُولَٰئِكَ to اولئك", () => {
-        expect(normalizeArabic("أُولَٰئِكَ")).toBe("اولئك");
-    });
+it.each([
+    { input: "", expected: "" },
+    { input: "عَلَىٰ", expected: "على" },
+    { input: "أَعْلَمُ", expected: "اعلم" },
+    { input: "الرَّحْمَٰنِ", expected: "الرحمن" },
+    { input: "اللَّهِ ۗ وَاللَّهُ", expected: "الله والله" },
+    { input: "أُولَٰئِكَ", expected: "اولئك" },
+  ])("normalizes '$input' as '$expected'", ({ input, expected }) => {
+    expect(normalizeArabic(input)).toBe(expected);
+  });
 });
