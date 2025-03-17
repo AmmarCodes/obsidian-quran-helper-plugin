13c55f2b8585a322f9cfaa04bf54c5de857f1275
Abdelhakim khabir <89462747+hakkm@users.noreply.github.com>
2025-03-17 03:39:00 +0000

    Fix normalization of Arabic text (#11)
    * fix replacing small alif with a normal alif
    
    * add testing for the normalize util function
    
    ---------
    
    Co-authored-by: Ammar Alakkad <am.alakkad@gmail.com>

---
+++ tests/utils.test.ts
@@ -0,0 +1,14 @@
+import { normalizeArabic } from "../src/utils";
+
+describe("normalizeArabicChars", () => {
+  it.each([
+    { input: "", expected: "" },
+    { input: "عَلَىٰ", expected: "على" },
+    { input: "أَعْلَمُ", expected: "اعلم" },
+    { input: "الرَّحْمَٰنِ", expected: "الرحمن" },
+    { input: "اللَّهِ ۗ وَاللَّهُ", expected: "الله والله" },
+    { input: "أُولَٰئِكَ", expected: "اولئك" },
+  ])("normalizes '$input' as '$expected'", ({ input, expected }) => {
+    expect(normalizeArabic(input)).toBe(expected);
+  });
+});
