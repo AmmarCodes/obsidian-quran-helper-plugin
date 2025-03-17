45a8b49883df3083c356914c8691e9061be09b5a
Ammar Alakkad <am.alakkad@gmail.com>
2025-03-17 08:32:34 +0300

    feat: Use fuzzy find Ayah in the ribbon command

--- main.ts
+++ main.ts
@@ -3,26 +3,26 @@ import { FzfAyahModal } from "src/FzfAyahModal";
 import { SurahModal } from "./src/SurahModal";
 
 export default class QuranHelper extends Plugin {
-	async onload() {
-		// This creates an icon in the left ribbon.
-		this.addRibbonIcon("book-open", "Insert Ayah", () => {
-			new SurahModal(this.app).open();
-		});
+  async onload() {
+    // creates an icon in the left ribbon.
+    this.addRibbonIcon("book-open", "Insert Ayah", () => {
+      new FzfAyahModal(this.app).open();
+    });
 
-		this.addCommand({
-			id: "open-quran-modal",
-			name: "Insert Ayah",
-			callback: () => {
-				new SurahModal(this.app).open();
-			},
-		});
-		this.addCommand({
-			id: "open-fzf-quran-modal",
-			name: "Fuzzy Find Ayah",
-			callback: () => {
-				new FzfAyahModal(this.app).open();
-			},
-		});
-	}
-	onunload() {}
+    this.addCommand({
+      id: "open-quran-modal",
+      name: "Insert Ayah",
+      callback: () => {
+        new SurahModal(this.app).open();
+      },
+    });
+    this.addCommand({
+      id: "open-fzf-quran-modal",
+      name: "Fuzzy Find Ayah",
+      callback: () => {
+        new FzfAyahModal(this.app).open();
+      },
+    });
+  }
+  onunload() {}
 }
