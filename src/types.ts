2190d908f4d507474fe66cbfb1e48773095b1480
Ahmed Hassan Mohamed Abdelrahman Alramah <125650424+alramah2003@users.noreply.github.com>
2026-02-20 06:18:21 +0000

    Add new properties to QuranHelperSettings and interfaces

--- src/types.ts
+++ src/types.ts
@@ -1,41 +1,36 @@
 export interface SearchableAyah {
-  surah_id: number;
-  ayah_id: number;
-  text: string;
-  surah_name: string;
-  surah_name_en: string;
+	  surah_id: number;
+	  ayah_id: number;
+	  text: string;
+	  surah_name: string;
 }
 
 export interface IndexedAyah extends SearchableAyah {
-  normalized_text: string;
+	  normalized_text: string;
 }
 
 export interface SearchableSurah {
-  id: number;
-  name: string;
-  transliteration: string;
-  type: string;
-  total_verses: number;
+	  id: number;
+	  name: string;
+	  transliteration: string;
+	  type: string;
+	  total_verses: number;
 }
 
 export interface IndexedSurah extends SearchableSurah {
-  normalized_name: string;
+	  normalized_name: string;
 }
 
 export interface QuranHelperSettings {
-  outputFormat: "blockquote" | "callout";
-  calloutType: string;
-  ayahNoteFolder: string;
-  ayahNotePathPattern:
-    | "surah-ayah"
-    | "surah/ayah"
-    | "arabic-ayah"
-    | "arabic/ayah";
+	  outputFormat: "blockquote" | "callout";
+	  calloutType: string;
+	  ayahNotePath: string;
+	  ayahNoteTags: string;
 }
 
 export const DEFAULT_SETTINGS: QuranHelperSettings = {
-  outputFormat: "callout",
-  calloutType: "quran",
-  ayahNoteFolder: "",
-  ayahNotePathPattern: "surah-ayah",
+	  outputFormat: "callout",
+	  calloutType: "quran",
+	  ayahNotePath: "Quran Notes",
+	  ayahNoteTags: "quran, ayah",
 };
