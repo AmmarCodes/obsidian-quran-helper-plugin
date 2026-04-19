173f039b2e33353ea4b2c8ec6e9ae39139c15450
mohamed medhat <mohamedmmedhat21@gmail.com>
2026-04-19 11:49:35 +0200

    add a divider line between Surahs in insert page cmd in callout format

--- src/FzfPageAyahModal.ts
+++ src/FzfPageAyahModal.ts
@@ -112,7 +112,10 @@ export class FzfPageAyahModal extends SuggestModal<PageAyahItem> {
     } else {
       const type = calloutType || "quran";
       content = `> [!${type}] ${header}\n> `;
-      ayahs.forEach((ayah) => {
+      ayahs.forEach((ayah, index) => {
+        if (index > 0 && ayah.surah_id !== ayahs[index - 1]?.surah_id) {
+          content += `\n>  ---\n> `;
+        }
         content += `${ayah.text} (${ayah.ayah_id}) `;
       });
       content += `\n`;
